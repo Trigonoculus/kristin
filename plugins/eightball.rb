1e353f00a7f1de80a14a6ad7563a3d50e3269c93
catskittens <cattuxxedo@gmail.com>
2016-11-06 13:26:10 +0000

    add eightball

---
+++ plugins/eightball.rb
@@ -0,0 +1,57 @@
+# MIT License
+#
+# Copyright (c) 2016 cats
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+module Kristin
+  class Plugin_Eightball < Plugin_Base
+
+    def execute(msg)
+      if cmd_match(msg, /(eightball|8ball) (.+)\?/)
+        ball_replies = ["It is certain.",
+          "It is decidedly so.",
+          "Without a doubt.",
+          "Yes, definitely.",
+          "You may rely on it.",
+          "As I see it, yes.",
+          "Most likely.",
+          "Outlook good.",
+          "Yes.",
+          "Signs point to yes.",
+          "Reply hazy try again.",
+          "Ask again later.",
+          "Better not tell you now.",
+          "Cannot predict now.",
+          "Concentrate and ask again.",
+          "Don't count on it.",
+          "My reply is no.",
+          "My sources say no.",
+          "Outlook not so good.",
+          "Very doubtful."
+        ]
+
+        Kristin.reply(@socket, msg[:from], msg[:nickname], ball_replies.sample)
+      else
+        Kristin.reply(@socket, msg[:from], msg[:nickname], "I cannot answer that, try asking a question.")
+      end
+    end
+
+  end
+end
